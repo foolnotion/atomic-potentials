0cb3366fd08349201920e61cec73eabf9487f82d
Bogdan Burlacu <bogdan.burlacu@fh-hagenberg.at>
2022-05-16 13:02:08 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,30 @@
 {
   description = "Atomic Potentials";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nur.url = "github:nix-community/NUR";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
-  inputs.operon.url = "github:heal-research/operon/cpp20";
-  inputs.pratt-parser.url = "github:foolnotion/pratt-parser-calculator?rev=a15528b1a9acfe6adefeb41334bce43bdb8d578c";
-  inputs.vstat.url = "github:heal-research/vstat/cpp20-eve";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nur.url = "github:nix-community/NUR";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
 
-  outputs = { self, flake-utils, nixpkgs, nur, operon, pratt-parser, vstat }:
+    operon.url = "github:heal-research/operon/cpp20";
+    vstat.url = "github:heal-research/vstat/cpp20-eve";
+    pratt-parser.url =
+      "github:foolnotion/pratt-parser-calculator?rev=a15528b1a9acfe6adefeb41334bce43bdb8d578c";
+    pypi-deps-db.url = "github:DavHau/pypi-deps-db";
+
+    mach-nix = {
+      #url = "github:DavHau/mach-nix";
+      url = "github:bjornfor/mach-nix/adapt-to-make-binary-wrapper";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        pypi-deps-db.follows = "pypi-deps-db";
+      };
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, nur, operon, pratt-parser, vstat
+    , pypi-deps-db, mach-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -16,6 +32,21 @@
           overlays = [ nur.overlay ];
         };
         repo = pkgs.nur.repos.foolnotion;
+        mach = mach-nix.lib.${system};
+
+        python-env = mach.mkPython {
+          requirements = ''
+            colorama
+            coloredlogs
+            numpy
+            optuna
+            pandas
+            sympy
+          '';
+
+          ignoreDataOutdated = true;
+        };
+
       in rec {
         defaultPackage = pkgs.gcc11Stdenv.mkDerivation {
           name = "atomic-potentials";
@@ -53,6 +84,9 @@
             pkg-config
             valgrind
             xxHash
+
+            # python tools
+            python-env
 
             # Some dependencies are provided by a NUR repo
             repo.aria-csv
